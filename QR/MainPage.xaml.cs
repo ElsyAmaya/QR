85a07e7a55f4ac38c540a8e69b085fcc9b6ebbf1
ElsyAmaya <pao.amayalazo01@gmail.com>
2023-08-05 13:25:14 -0600

    Agregar archivos de proyecto.

---
+++ QR/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+
+namespace QR
+{
+    public partial class MainPage : ContentPage
+    {
+        ZXingBarcodeImageView qr;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+     
+        private async void ButtonGenerarQRClicked(object sender, EventArgs e)
+        {
+            qr = new ZXingBarcodeImageView
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+            };
+            qr.BarcodeFormat = ZXing.BarcodeFormat.QR_CODE;
+            qr.BarcodeOptions.Width = 500;
+            qr.BarcodeOptions.Height = 500;
+            qr.BarcodeValue = "https://youtu.be/q1f2vzn8xbA";
+            stKQR.Children.Add(qr);
+
+           
+        }
+
+
+       
+
+    }
+}
